9fce0a68fca89d2612b0564accee4be6f1e1ccc5
APC9 <penacastilloalberto32@gmail.com>
2024-07-17 14:53:49 +0200

    AWS-SDK-V3

--- src/task.js
+++ src/task.js
@@ -1,132 +1,123 @@
-const { v4 } = require('uuid')
-const AWS = require('aws-sdk')
+import { 
+  DeleteItemCommand,
+  DynamoDBClient, 
+  GetItemCommand, 
+  PutItemCommand, 
+  ScanCommand, 
+  UpdateItemCommand 
+} from "@aws-sdk/client-dynamodb"; 
+import { v4 } from 'uuid'
 
-const addTask = async (event) => {
-  const dynamodb = new AWS.DynamoDB.DocumentClient();
+const TableName = 'APC-taskTable'
 
-  const { title, description } = JSON.parse(event.body)
-  const createdAt = new Date()
-  const id = v4()
-
-  const newTask = {
-    id,
-    title,
-    description,
-    createdAt,
-    done: false
-  }
-
-  await dynamodb.put({
-    TableName: 'APC-taskTable',
-    Item: newTask
-  }).promise()
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify(newTask)
-  }
-}
-
-const getTasks = async (event) => {
-  const dynamodb = new AWS.DynamoDB.DocumentClient();
-
-  try {
-    const result = await dynamodb.scan({
-      TableName: 'APC-taskTable',
-    }).promise()
+export const addTask = async (event) => {
+  const client = new DynamoDBClient()
   
-    const tasks = result.Items
-  
-    return {
-      statusCode: 200,
-      body: JSON.stringify(tasks)
-    }
-  } catch (error) {
-    console.log({error})
-  }
-
-}
-
-const getTask = async (event) => {
-  const dynamodb = new AWS.DynamoDB.DocumentClient();
-
-  try {
-    const { id } = event.pathParameters;
-
-    const result = await dynamodb.get({
-      TableName: 'APC-taskTable',
-      Key: {
-        id     
-      }
-    }).promise()
+  try {    
+    const { title, description } = JSON.parse(event.body)
+    const createdAt = new Date().toISOString();
+    const id = v4()
     
-    return {
-      statusCode: 200,
-      body: JSON.stringify(result.Item)
+    const Item = {
+      id: { S: id },
+      title: { S: title },
+      description: { S: description },
+      createdAt: { S: createdAt },
+      done: { BOOL: false }
     }
+  
+    const input = {
+      TableName,
+      Item,
+      ReturnValues: "ALL_OLD",
+       ReturnItemCollectionMetrics: "SIZE"
+    }
+    
+    const command = new PutItemCommand(input)
+    return await client.send(command)
+
   } catch (error) {
-    console.log({error})
+    console.error("Error adding task:", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Failed to add task" })
+    };
   }
+}
+
+
+export const getTasks = async () => {
+  const client = new DynamoDBClient()
+
+  const input = {
+    TableName,
+    Limit: Number("int"),
+    TotalSegments: Number("int"),
+    Segment: Number("int"),
+  }
+
+  const command = new ScanCommand(input);
+  return await client.send(command);
 
 }
 
-const updateTask = async (event) => {
-  const dynamodb = new AWS.DynamoDB.DocumentClient();
+export const getTask = async (event) => {
+  const client = new DynamoDBClient()
+  const { id } = event.pathParameters
   
+  const input = {
+    TableName,
+    Key: {
+      id: { S: id }
+    }
+  }
+
+  const command = new GetItemCommand(input);
+  return await client.send(command);
+}
+
+export const updateTask = async (event) => {
+  const client = new DynamoDBClient()
+
   try {
     const { id } = event.pathParameters;
     const { done, title, description } = JSON.parse(event.body)
     
-    const result = await dynamodb.update({
-      TableName: 'APC-taskTable',
+    const input = {
+      TableName,
       Key: {
-        id     
+        id: { S: id }
       },
       UpdateExpression: 'set done = :done, title = :title, description = :description',
       ExpressionAttributeValues: {
-        ':done' : done,
-        ':title' : title,
-        ':description' : description
+        ':done': { BOOL: done },
+        ':title': { S: title },
+        ':description': { S: description }
       },
       ReturnValues: 'ALL_NEW'
-    }).promise()
+    };
     
-    return {
-      statusCode: 200,
-      body: "update successfully"
-    }
+    const command = new UpdateItemCommand(input);
+    return await client.send(command);
+
   } catch (error) {
     console.log({error})
   }
   
 }
 
-const deleteTask = async (event) => {
-  const dynamodb = new AWS.DynamoDB.DocumentClient();
-
-  try {
-    const { id } = event.pathParameters;
-
-    const result = await dynamodb.delete({
-      TableName: 'APC-taskTable',
-      Key: {
-        id     
-      }
-    }).promise()
-    
-    return {
-      statusCode: 200,
-      body: "delete successfully"
-    }
-  } catch (error) {
-    console.log({error})
+export const deleteTask = async (event) => {
+  const client = new DynamoDBClient()
+  const { id } = event.pathParameters
+  
+  const input = {
+    TableName,
+    Key: {
+      id: { S: id }
+    },
+    ReturnValues: "ALL_OLD"
   }
 
+  const command = new DeleteItemCommand(input);
+  return await client.send(command);
 } 
-module.exports = {
-  addTask,
-  getTasks,
-  getTask,
-  updateTask,
-  deleteTask
-}
